62088b21740ca9d51a198f24a8d4545f722d5633
Sam <samuelmatthews96@gmail.com>
2020-06-30 19:21:20 +0100

    Added templates folder and loaded index

--- website/proprisk/views.py
+++ website/proprisk/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
 
-# Views
+from .models import Question
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-    #return render(request, 'proprisk/index.html')
+    return render(request, 'proprisk/index.html')    
